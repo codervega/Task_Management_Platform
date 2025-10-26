73be36f1d278d72885c7744d05b6b39d78b1f287
Abhishek shukla <151427673+codervega@users.noreply.github.com>
2025-10-26 19:38:27 +0530

    Delete back-end/express directory

--- back-end/express/noteApp/config/database.js
+++
@@ -1,7 +0,0 @@
-let mongoose = require("mongoose")
-const url = 'mongodb://127.0.0.1:27017/noteApp'
-
-exports.connectdb = async()=>{
-  await mongoose.connect(url)
-  console.log("data base is Connected");
-}
